a214356a806dbe1771003cbb314d37e7bc6d1abb
Benjamin Key <benjamin.key@student.tafesa.edu.au>
2024-03-03 18:41:10 +1030

    Mortgage feature branch development complete

--- UniversalCalculator/MortgageCalculator.xaml.cs
+++ UniversalCalculator/MortgageCalculator.xaml.cs
@@ -7,11 +7,13 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using System.Windows;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -45,6 +47,53 @@ namespace Calculator
 		private void exit2Button_Click(object sender, RoutedEventArgs e)
 		{
 			Frame.Navigate(typeof(MainMenu));
+		}
+
+		private async void calcButton_Click(object sender, RoutedEventArgs e)
+		{
+			double principal;
+			int years;
+			int months;
+			double yearlyInterest;
+			int totalMonths;
+			double monthlyInterest;
+
+			try
+			{
+				principal = double.Parse(principalTextBox.Text);
+				years = int.Parse(yearsTextBox.Text);
+				months = int.Parse(monthsTextBox.Text);
+				yearlyInterest = double.Parse(yearlyInterestTextBox.Text);
+			}
+			catch
+			{
+				var dialog = new MessageDialog("Error! Please enter a valid data.");
+				await dialog.ShowAsync();
+				principalTextBox.Focus(FocusState.Programmatic);
+				principalTextBox.SelectAll();
+				return;
+			}
+
+			totalMonths = years * 12 + months;
+			monthlyInterest = yearlyInterest / 12 / 100;
+
+			double output = CalculateMonthlyRepayment(principal, totalMonths, monthlyInterest);
+
+			repaymentTextBox.Text = output.ToString("N2");
+			monthlyInterestTextBox.Text = (monthlyInterest * 100).ToString("N2");
+
+		}
+
+		private void exit4Button_Click(object sender, RoutedEventArgs e)
+		{
+			Frame.Navigate(typeof(MainMenu));
+		}
+
+		public static double CalculateMonthlyRepayment(double principal, int totalMonths, double monthlyInterestRate)
+		{
+			double M = principal * (monthlyInterestRate * Math.Pow(1 + monthlyInterestRate, totalMonths)) / (Math.Pow(1 + monthlyInterestRate, totalMonths) - 1);
+
+			return M;
 		}
 	}
 }
